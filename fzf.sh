0237356994f6eed5c1babf3e9ad582d1652f274e
tomocy <tomocy.dev@gmail.com>
2020-09-29 16:59:16 +0900

    add f

--- fzf.sh
+++ fzf.sh
@@ -1,25 +1,28 @@
 #!/bin/zsh
 
-ffind() {
-    local maxdepth_opt='-maxdepth 1'
-    local type_opt
-    while getopts d:t: opt
-    do
-        case $opt in
-            d) maxdepth_opt="-maxdepth $OPTARG";;
-            t) type_opt="-type $OPTARG";;
-        esac
-    done
+export FZF_DEFAULT_OPTS="--extended --cycle --tac --ansi --multi"
+export TOMOCY_FZF_DEFAULT_COMMAND_OPTS='--hidden --follow --exclude .git'
+export FZF_DEFAULT_COMMAND='fd --type f $TOMOCY_FZF_DEFAULT_COMMAND_OPTS'
 
-    echo "$(find * $(echo $maxdepth_opt) $(echo $type_opt) -print 2> /dev/null | fzf-tmux)"
+_fzf_compgen_path() {
+  fd $(echo $TOMOCY_FZF_DEFAULT_COMMAND_OPTS) . "$1"
+}
+
+_fzf_compgen_dir() {
+  fd --type d $(echo $TOMOCY_FZF_DEFAULT_COMMAND_OPTS) . "$1"
+}
+
+f() {
+    local arg=$(fd $(echo $TOMOCY_FZF_DEFAULT_COMMAND_OPTS) ${@:2} | fzf) && test -n "$arg" && print -z -- "$1 $arg"
 }
 
 _fcd() {
-    local dir=$(ffind $(echo $maxdepth_opt) -t d) && cd "$dir"
+    args=$(echo "$@" | awk '{gsub("-t [a-zA-Z]|--type [a-zA-Z]", "", $0);print $0}' | xargs)
+    f cd $(echo $args) --type d
 }
 
 _fcpd() {
-  local declare dirs=()
+  local dirs=()
   get_parent_dirs() {
     if [[ -d "$1" ]]
     then
@@ -30,16 +33,16 @@ _fcpd() {
 
     if [[ "$1" == '/' ]]
     then
-        for _dir in ${dirs[@]}
+        for dir in ${dirs[@]}
         do
-            echo $_dir
-        done
+            echo $dir
+        done | tac
     else
         get_parent_dirs $(dirname $1)
     fi
   }
 
-  local dir=$(get_parent_dirs $(realpath ${1:-$PWD}) | fzf-tmux --tac) && cd "$dir"
+  local dir=$(get_parent_dirs $(realpath ${1:-$PWD}) | fzf) && cd $dir
 }
 
 
